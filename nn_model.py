50384767ddd2a6ede686952d1936eb77ee92a570
G <chigigupta003@gmail.com>
2026-03-29 15:15:53 +0530

    Add files via upload

---
+++ nn_model.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+class SIRNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(3, 128),
+            nn.Tanh(),
+            nn.Linear(128, 128),
+            nn.Tanh(),
+            nn.Linear(128, 3)
+        )
+
+    def forward(self, x):
+        return self.net(x)
